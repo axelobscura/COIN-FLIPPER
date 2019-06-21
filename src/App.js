6221b0d41451885136a157f3831b592d79c40a13
Axel <axosar@gmail.com>
2019-06-21 12:44:20 -0500

    Adding Helpers
    Adding a helpers file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CoinContainer from './CoinContainer';
 
 function App() {
   return (
     <div className="App">
-      
+      <CoinContainer />
     </div>
   );
 }
